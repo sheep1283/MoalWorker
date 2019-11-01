d0f0dd10599fef7b62a26902dbe3f5ff362d62a9
sheep1283 <sheep1283@naver.com>
2019-11-01 16:01:07 +0900

    Add BottomNavigation(Tabbed)

--- app/src/main/java/com/example/moal_worker/CalenderActivity.kt
+++ app/src/main/java/com/example/moal_worker/CalenderActivity.kt
@@ -2,9 +2,14 @@ package com.example.moal_worker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.calender_activity.*
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CalenderActivity : AppCompatActivity(), BottomSheet.BottomSheetListener {
+    lateinit var homeFragment: HomeFragment
+    lateinit var secondFragment: SecondFragment
+    lateinit var thirdFragment: ThirdFragment
+
     override fun onOptionClick(text: String) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -13,11 +18,53 @@ class CalenderActivity : AppCompatActivity(), BottomSheet.BottomSheetListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calender_activity)
 
-            button_open_bottom_sheet.setOnClickListener {
-            val bottomSheet = BottomSheet()
-            bottomSheet.show(supportFragmentManager, "BottomSheet")
+        val bottomnavigation : BottomNavigationView = findViewById(R.id.btm_nav)
 
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout,homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        bottomnavigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home->{
+
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+                R.id.second->{
+
+                    secondFragment = SecondFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,secondFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+                R.id.third->{
+
+                    thirdFragment = ThirdFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,thirdFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+
+                }
+            }
+            true
         }
+
+
+
+
 
 
     }
