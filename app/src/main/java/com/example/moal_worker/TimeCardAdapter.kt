23335ecfa72b4405944170d046f28964c428ce47
polcomlicepute <min199912@naver.com>
2019-11-21 18:19:57 +0900

    apply (show plans from DB to day calendar) to HomeFragment

--- app/src/main/java/com/example/moal_worker/TimeCardAdapter.kt
+++ app/src/main/java/com/example/moal_worker/TimeCardAdapter.kt
@@ -10,8 +10,7 @@ import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.day_calendar.*
 import kotlinx.android.synthetic.main.part_time_cardview.view.*
 
-class TimeCardAdapter(
-    val timeList:ArrayList<JobTimeForReading>, val listOfDay:ArrayList<DayScheduleModel>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class TimeCardAdapter(val timeList:ArrayList<JobTimeForReading>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeCardAdapter.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -26,48 +25,6 @@ class TimeCardAdapter(
         val holder = viewHolder as ViewHolder
         val data: JobTimeForReading = timeList[position]
         holder.bind(data)
-
-
-
-
-        val day = data.jobDay
-        var dayInt = 0
-        when (day) {
-            "월" -> dayInt = 1
-            "화" -> dayInt = 2
-            "수" -> dayInt = 3
-            "목" -> dayInt = 4
-            "금" -> dayInt = 5
-            "토" -> dayInt = 6
-            "일" -> dayInt = 0
-        }
-        //i++
-        //dayList.add(dayInt)
-
-        val positionName: String = data.positionName
-        val partName: String = data.partName
-        val startHour = data.startHour
-        val startMin :Float = (((data.startMin) / 6)*0.1).toFloat()
-        val endHour = data.endHour
-        val endMin: Float = (((data.endMin)/ 6)*0.1).toFloat()
-        val timeInt: Float = 0.5F
-        var start: Float = (startHour + startMin).toFloat()
-        val end: Float = endHour + endMin
-        val viewnum: Int = 2 * (end - start).toInt()
-        var t: Int = 0 //listofDay 인덱스 변수
-        while (start < end) {
-
-
-
-            t = dayInt + (7 * 2 * start).toInt()
-            //dayModel = DayScheduleModel()
-            listOfDay[t] = DayScheduleModel(positionName, partName, Color.rgb(240, 0, 0))
-            start = (start + timeInt)
-        }
-
-
-
-        holder.toAdapter(listOfDay)
 
 
 
@@ -91,6 +48,7 @@ class TimeCardAdapter(
             itemView.cardView_day.text = data.jobDay + "요일"
 
             itemView.Layout.setOnClickListener {
+                if(data.storeName != null){ //임시 null허용 처리
                 database.child(data.storeName).child("WorkingPart")
                     .child(data.jobDay)
                     .child(data.positionName)
@@ -99,17 +57,7 @@ class TimeCardAdapter(
                     .child("J ini")
                     .setValue("Request")
 
-            }
-        }
-
-        val v = inflater.inflate(R.layout.day_calendar, parent, false)
-        // val include_cal_view : View = v.findViewById(R.id.cal_view_1)
-        val day: RecyclerView = v.findViewById(R.id.day_sche_calendar)
-
-        fun toAdapter(listOfDay: ArrayList<DayScheduleModel>) {
-            val dayListAdapter = DayListAdapter()
-            day.adapter = dayListAdapter
-            dayListAdapter.setDayList(listOfDay)
+            }}
         }
 
 
